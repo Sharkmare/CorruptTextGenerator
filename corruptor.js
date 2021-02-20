dfc9d52d5a5cb6abbb37deb71356179d6d3752c7
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2021-02-20 09:49:16 +0100

    Initial commit V1

---
+++ corruptor.js
@@ -0,0 +1,20 @@
+function corruptme(r) {
+    for (corrupters = ["0", "+", "#", "@", "&", "%", "$", "§", "!"], Amax = r.length, ammount = Amax / 5, i = 0; i < ammount; i++) {
+        let t = corrupters[rng(corrupters.length)],
+            n = rng(Amax);
+        " " != r[n] ? r = rep(r, n, t) : i--
+    }
+    return console.log(r)
+}
+
+function rng(r) {
+    return Math.floor(Math.random() * r)
+}
+
+function rep(r, t, n) {
+    return r = setCharAt(r, t, n)
+}
+
+function setCharAt(r, t, n) {
+    return t > r.length - 1 ? r : r.substring(0, t) + n + r.substring(t + 1)
+}
